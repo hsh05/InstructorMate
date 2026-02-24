d42896d4a30f04cd469aad4464d015b42effd8ec
Merna90 <marnoon2003@gmail.com>
2026-02-24 22:48:39 +0400

    fix app_cofig

--- lib/config/app_config.dart
+++ lib/config/app_config.dart
@@ -7,7 +7,7 @@
 class AppConfig {
   //https://instructormate1.onrender.com
   // FIX: Change this to your server IP/hostname before deploying.
-  static const String baseUrl = "http://10.0.2.15:8000";
+  static const String baseUrl = "https://instructormate1.onrender.com";
 
   // Per-request timeouts (formerly hardcoded inline in api_client.dart)
   static const Duration shortTimeout = Duration(seconds: 20);
